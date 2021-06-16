afbae2fd919e7e7751f44348b644fa9f19f5afdd
Dominik Dewitz <dewitzdominik@kabelmail.de>
2021-06-16 17:41:16 +0200

    Delete unneccessary client variables

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,3 @@
-require "base64"
-
 class PagesController < ApplicationController
   def index
     if params[:code].present?
@@ -33,11 +31,8 @@ class PagesController < ApplicationController
     @base_url = "https://accounts.spotify.com/authorize"
     @redirect_uri = "http://localhost:3000/index"
 
-    @client_id = ENV['CLIENT_ID']
-    @client_secret = ENV['CLIENT_SECRET']
-
     @scope = "playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative"
 
-    @url = "#{@base_url}?client_id=#{@client_id}&response_type=code&redirect_uri=#{@redirect_uri}&show_dialog=true&scope=#{@scope}"
+    @url = "#{@base_url}?client_id=#{ENV['CLIENT_ID']}&response_type=code&redirect_uri=#{@redirect_uri}&show_dialog=true&scope=#{@scope}"
   end
 end
